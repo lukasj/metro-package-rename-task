35f74dc5cb342366c66d84977d63eb524178489f
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-12-03 16:03:58 +0100

    update plugin
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- package-rename-task/src/main/java/com/sun/wts/tools/ant/PackageRenameMojo.java
+++ package-rename-task/src/main/java/com/sun/wts/tools/ant/PackageRenameMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -19,6 +19,10 @@ import java.util.Map.Entry;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.apache.tools.ant.DefaultLogger;
 import org.apache.tools.ant.Project;
@@ -27,37 +31,35 @@ import org.apache.tools.ant.Project;
  * Creates renamed source files under the specified directory
  * (defaults to <tt>target/generated-sources/renamed-sources</tt>)
  *
- * @goal rename
- * @phase generate-sources
- * @requiresProject
- * @requiresDependencyResolution runtime
+ * Goal: rename
+ * Phase: generate-sources
+ * RequiresProject: true
+ * RequiresDependencyResolution: runtime
  *
  * @author Kohsuke Kawaguchi
  * @author Lukas Jungmann
  */
+@Mojo(name = "rename", requiresProject = true, defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresDependencyResolution = ResolutionScope.RUNTIME)
 public class PackageRenameMojo extends AbstractMojo {
 
     /**
      * The maven project.
      *
-     * @parameter expression="${project}"
-     * @required
-     * @readonly
      */
+    @Parameter(required = true, property = "project", readonly = true)
     protected MavenProject project;
 
     /**
      * The root directory to put the renamed source files.
-     *
-     * @parameter expression="${renamedSourcesDir}" default-value="target/generated-sources/renamed-sources"
      */
+    @Parameter(required = true, property = "renamedSourcesDir", defaultValue = "${project.build.directory}/generated-sources/renamed-sources")
     private File rootDir;
 
     /**
      * The root directory to put the renamed source files.
      *
-     * @parameter expression="${srcSourcesDir}" default-value="${project.build.directory}/dependency"
      */
+    @Parameter(required = true, property = "srcSourcesDir", defaultValue = "${project.build.directory}/dependency")
     private File srcDir;
 
     /**
@@ -69,9 +71,8 @@ public class PackageRenameMojo extends AbstractMojo {
      * {@code <com.sun.xml>java.xml.bind/com.sun.xml.internal</com.sun.xml>}
      *
      * Will be renamed to com.sun.xml.internal and put into java.xml.bind directory.
-     *
-     * @parameter
      */
+    @Parameter
     private Map patterns;
 
     /**
@@ -79,6 +80,7 @@ public class PackageRenameMojo extends AbstractMojo {
      *
      * @parameter
      */
+    @Parameter
     private String excludes;
 
     @Override
